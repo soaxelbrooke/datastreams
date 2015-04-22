b120ade1e3f892b0be1b0e224290c225f9a86344
Stuart Axel Owen <stuart.owen@gmail.com>
2015-04-22 05:49:45 +0000

    Add setup and license

---
+++ setup.py
@@ -0,0 +1,16 @@
+__author__ = 'stuart'
+
+from distutils.core import setup
+
+setup(
+    name='datastreams',
+    packages=['datastreams'],
+    version='0.1',
+    description='Efficient, concise stream data processing.',
+    author='Stuart Owen',
+    author_email='stuart.owen@gmail.com',
+    url='https://github.com/StuartAxelOwen/datastreams',
+    download_url='https://github.com/StuartAxelOwen/datastreams/tarball/0.1',
+    keywords=['stream', 'streams', 'streaming', 'map', 'reduce', 'mapreduce', 'spark', 'functional'],
+    classifiers=[],
+)
